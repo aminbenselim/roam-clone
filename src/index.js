e63067cd7e8027167fb10ba11b1fe2b85ea17bad
amin benelim <aminebenselim@gmail.com>
2021-01-23 19:29:54 +0100

    fix dgraph import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QueryCache, ReactQueryCacheProvider } from "react-query";
-
+import * as dgraph from 'dgraph-js-http';
 import App from "./App";
 
 const DQL_ENDPOINT = "http://localhost:8080";
 const queryCache = new QueryCache();
 
-const dgraph = require("dgraph-js-http");
 const clientStub = new dgraph.DgraphClientStub(
   DQL_ENDPOINT
 );
